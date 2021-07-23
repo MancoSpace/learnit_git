3adb15a11df4b7f5c395f1f20627823c844a8ab4
Vishal Kumar Mishra <vishalkumarmishra8052@gmail.com>
2021-07-23 17:17:21 +0530

    init commit

--- Lrc/a.java
+++ Lrc/a.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsHelloWorldApplication {
 
 	public static void main(String[] args) {
+		printl("hello Worl");
 		SpringApplication.run(AwsHelloWorldApplication.class, args);
 	}
 
