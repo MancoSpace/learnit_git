d38bff0b8717ef69a72f1a3e2e6e271c524d7a5f
Vishal Kumar Mishra <vishalkumarmishra8052@gmail.com>
2021-07-23 17:20:24 +0530

    new changes

--- Lrc/a.java
+++ Lrc/a.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsHelloWorldApplication {
 
 	public static void main(String[] args) {
-		printl("hello Worl");
+		printl("hello Worlf");
 		SpringApplication.run(AwsHelloWorldApplication.class, args);
 	}
 
